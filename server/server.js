5c935d1496099538f2cc1940fef583b143da7081
Promise Johnson <chiemelapromise30@gmail.com>
2021-12-31 10:23:09 +0100

    Deployment

--- server/server.js
+++ server/server.js
@@ -20,9 +20,9 @@ app.use(helmet());
 if (process.env.NODE_ENV === "production"){
   app.enable("trust proxy");
 
-  app.use(express.static("../client/build"));
+  app.use(express.static("./build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "../client/build/index.html"));
+    res.sendFile(path.resolve(__dirname,  "./build/index.html"));
   });
 }
 
